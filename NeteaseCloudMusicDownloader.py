0ba94450e1d87176e79839776761e06231310ccb
HerramadeusGameAppLib <41694742+HerramadeusGameAppLib@users.noreply.github.com>
2018-12-01 02:00:58 +0800

    Update NeteaseCloudMusicDownloader.py

--- NeteaseCloudMusicDownloader.py
+++ NeteaseCloudMusicDownloader.py
@@ -1,60 +1,62 @@
-
+#-------------------------------------------------------------
 #-------------------------------------------------------------
 
 class UrlChecker:
 
-    def IsValidUrl(self, url, error_message):
+    def IsValidUrl(self, url):
         return NotImplemented
 
 #-------------------------------------------------------------
 
 class KeywordSongChecker(UrlChecker):
 
-    def IsValidUrl(self, url, error_message):
+    def IsValidUrl(self, url):
 
         if url.rfind("song") >= 0 :
             return True
 
-        error_message = "no keyword: \"song\""
+        print("Invalid Url: No Keyword: \"song\"")
         return False
 
 #-------------------------------------------------------------
 
 class NetEaseMusicLinkChecker(UrlChecker):
     
-    def IsValidUrl(self, url, error_message):
+    def IsValidUrl(self, url):
 
         if url.find("music.163.com") >= 0 :
             return True
 
-        error_message = "not a net ease music link"
+        print("Invalid Url: Not A Netease Cloud Music Link")
         return False
 
+#-------------------------------------------------------------
 #-------------------------------------------------------------
 
 class MusicIDGetter:
     
     url_checkers = None
 
-    def GetMusicID(self, url, error_message):
+    def GetMusicID(self, url):
 
         if None != self.url_checkers:
             for checker in self.url_checkers:
-                if False == checker.IsValidUrl(url, error_message):
+                if False == checker.IsValidUrl(url):
                     return None
 
         music_id_str = str(url).rpartition("id=")[2]
         if(id(music_id_str) == id(url)):
-            error_message = "no keyword: \"id\""
+            print("Invalid Url: No Keyword: \"id\"")
             return None
 
         if(0 == len(music_id_str)):
-            error_message = "no valid id"
+            print("Invalid Url: No Valid ID")
             return None
 
         print("Music ID: " + music_id_str)
         return music_id_str
 
+#-------------------------------------------------------------
 #-------------------------------------------------------------
 
 class OutLinkUrlGetter:
@@ -62,14 +64,15 @@ class OutLinkUrlGetter:
     out_link_url_head = "http://music.163.com/song/media/outer/url?id="
     out_link_url_tail = ".mp3"
 
-    def GetOutLinkUrl(self, music_id_str, error_message):
+    def GetOutLinkUrl(self, music_id_str):
         
         if None == music_id_str or 0 == len(music_id_str):
-            error_message = "invalid music id"
+            print("Invalid Music ID")
             return None
 
         return self.out_link_url_head+music_id_str+self.out_link_url_tail
 
+#-------------------------------------------------------------
 #-------------------------------------------------------------
 
 import urllib
@@ -77,9 +80,11 @@ import urllib.request
 
 class ActualUrlGetter:
 
-    def GetActualUrl(self, outlink_url, error_message):
+    def GetActualUrl(self, outlink_url):
 
         result = None
+        res = None
+
         try:
             req = urllib.request.Request(url=outlink_url, method="GET")
             req.add_header("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)")
@@ -88,10 +93,28 @@ class ActualUrlGetter:
             result = res.geturl()
 
         except Exception as ex:
-            error_message = str(ex)
-        
+            print("Cannot Get Actual Url: " + str(ex))
+
+        finally:
+            if None != res:
+                res.close()
+
         return result
-        
+
+#-------------------------------------------------------------
+#-------------------------------------------------------------
+
+class ActualUrl_404_Checker(UrlChecker):
+
+    url_of_404 = "404"
+
+    def IsValidUrl(self, url):
+        if str(url).rfind(self.url_of_404) + 3 < len(url):
+            return True
+
+        print("Not An Downloadable Url: Actual Url is 404")
+
+#-------------------------------------------------------------        
 #-------------------------------------------------------------
 
 class MusicDataWriter:
@@ -104,27 +127,58 @@ class MusicDataWriter:
 class SimpleMusicDataWriter(MusicDataWriter):
 
     def Write(self, path_to_write, data):
-        with open(path_to_write, "wb") as write_file:
-            write_file.write(data)
 
+        result = False
+        try:
+            write_file = open(path_to_write, "wb")
+            write_file.write(data)
+            result = True
+        except ex:
+            print("Cannot Write Music Data: " + str(ex))
+
+        finally:
+            if None != write_file:
+                write_file.close()
+
+        return result
+
+#-------------------------------------------------------------
 #-------------------------------------------------------------
 
 class Downloader:
     def Download(self, url, path_to_write):
         return NotImplemented
 
+#-------------------------------------------------------------
+
 class HttpDownloader(Downloader):
 
     music_data_writer = None
 
     def __init__(self, *args, **kwargs):
+
         self.music_data_writer = SimpleMusicDataWriter()
         return super().__init__(*args, **kwargs)
 
     def Download(self, url_to_download, path_to_write):
-        data = urllib.request.urlopen(url_to_download).read()
-        self.music_data_writer.Write(path_to_write, data)
 
+        result = False
+        try:
+            req = urllib.request.urlopen(url_to_download)
+            data = req.read()
+            self.music_data_writer.Write(path_to_write, data)
+            result = True
+
+        except ex:
+            print("Cannot Download: " + str(ex))
+
+        finally:
+            if None != req:
+                req.close()
+
+        return result
+
+#-------------------------------------------------------------
 #-------------------------------------------------------------
 
 import sys
@@ -132,7 +186,10 @@ class DownloadPerformer:
 
     music_id_getter = None
     outlink_url_getter = None
+
     actual_url_getter = None
+    actual_url_404_checker = None
+
     downloader = None
 
     def __init__(self, *args, **kwargs):
@@ -141,37 +198,40 @@ class DownloadPerformer:
 
         self.outlink_url_getter = OutLinkUrlGetter()
         self.actual_url_getter = ActualUrlGetter()
+        self.actual_url_404_checker = ActualUrl_404_Checker()
         self.downloader = HttpDownloader()
 
     def  Run(self):
-        music_url = input("Input Music Url:")
-        error_message = ""
+        music_url = input("\nInput Music Url:")
 
-        music_id = self.music_id_getter.GetMusicID(music_url, error_message)
+        print("Getting Music ID...")
+        music_id = self.music_id_getter.GetMusicID(music_url)
         if None == music_id:
-            print("Invalid Song ID: " + error_message)
-            print("\n")
             return
 
-        outlink_url = self.outlink_url_getter.GetOutLinkUrl(music_id, error_message)
-    
+        print("Getting Outlink Url...")
+        outlink_url = self.outlink_url_getter.GetOutLinkUrl(music_id)
         if None == outlink_url:
-            print("Invalid Outlink: " + error_message)
-            print("\n")
             return
 
-        actual_url = self.actual_url_getter.GetActualUrl(outlink_url, error_message)
-        print("Actual Url Is:\n")
-        print(actual_url)
+        print("Getting Actual Url...")
+        actual_url = self.actual_url_getter.GetActualUrl(outlink_url)
+        if None == actual_url:
+            return
+
+        if not self.actual_url_404_checker.IsValidUrl(actual_url):
+            return
+
+        #-------------------------------------------------------------
+
+        print("Actual Url Is:\n\n" + actual_url)
         print("\nStart Downloading...\n")
 
         path_to_write = sys.path[0]+"\\"+music_id+".mp3"
-        self.downloader.Download(actual_url, path_to_write)
+        if self.downloader.Download(actual_url, path_to_write):
+            print("Download Completed At:\n" + path_to_write + "\n")
 
-        print("Download Completed At:\n")
-        print(path_to_write)
-        print("\n")
-
+#-------------------------------------------------------------
 #-------------------------------------------------------------
 
 download_performer = DownloadPerformer()
